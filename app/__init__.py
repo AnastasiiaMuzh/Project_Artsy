b87dbee1e8bd6e59e0f526cf2588d3fcc2a8aca5
AnastasiiaMuzh <anastasiia.muzhzhavlev@outlook.com>
2025-01-30 14:47:11 -0700

    Completed shopping cart route

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
+from .api.shopping_cart_items import cart_routes
 
 app = Flask(__name__, static_folder='../react-vite/dist', static_url_path='/')
 
@@ -30,6 +31,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 db.init_app(app)
 Migrate(app, db)
+app.register_blueprint(cart_routes, url_prefix='/api/cart')
 
 # Application Security
 CORS(app)

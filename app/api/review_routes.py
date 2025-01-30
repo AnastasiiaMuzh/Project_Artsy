9a6ddf7875a3dc90d8a3bed92761fbabb3991001
Erika <brandonerika96@gmail.com>
2025-01-30 00:56:42 -0500

    split review and review image routes

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -159,38 +159,6 @@ def create_review(id):
     }), 201
 
 
-
-# ***********************CREATE Review Image***********************
-@review_routes.route('/<int:id>', methods=['POST'])
-@login_required
-def add_review_image(id):
-    review = Review.query.get(id)
-    if not review:
-        return {"message": "Review couldn't be found"}, 404
-
-    if review.buyerId != current_user.id:
-        return jsonify({"message": "Unauthorized"})
-
-    review_images = ReviewImage.query.filter_by(reviewId=id).all()
-    if len(review_images) == 10:
-        return jsonify({ "message": "Maximum number of images for this resource was reached" }), 403
-
-    data = request.get_json()
-    review_url = data.get('url')
-
-    new_review_image = ReviewImage(
-        reviewId=review.id,
-        url=review_url
-    )
-    db.session.add(new_review_image)
-    db.session.commit()
-
-    return jsonify({
-        "id": review.id,
-        "url": review_url
-    }), 201
-
-
 # ***********************EDIT Review***********************
 @review_routes.route('/<int:id>', methods=['PATCH'])
 @login_required
@@ -237,26 +205,6 @@ def delete_review(id):
         return {"message": "Review couldn't be found"}, 404
 
     db.session.delete(review)
-    db.session.commit()
-
-    return jsonify({ "message": "Successfully deleted" }), 200
-
-
-# ***********************DELETE Review Image***********************
-@review_routes.route('/images/<string:url>', methods=['DELETE'])
-@login_required
-def delete_review(url):
-    review_image = ReviewImage.query.filter_by(url=url).first()
-    if not review_image:
-        return {"message": "Review Image couldn't be found"}, 404
-
-    review = Review.query.filter_by(id=review_image.reviewId).first()
-    if not review:
-        return {"message": "Review couldn't be found"}, 404
-    if review.buyerId != current_user.id:
-        return jsonify({"message": "Unauthorized"})
-
-    db.session.delete(review_image)
     db.session.commit()
 
     return jsonify({ "message": "Successfully deleted" }), 200
